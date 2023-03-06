8d85e10a039155f13014179df953cfc52d01ec7d
hsalava1 <hsalava@liveparallel.com>
2023-03-06 15:48:54 -0500

    deleted 3 wrong tables

---
+++ BI-EDW-DDL/Audit.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Audit]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ BI-EDW-DDL/ItemSAL.sql
@@ -0,0 +1,15 @@
+﻿use DataVault
+go
+
+create table BV.ItemSAL
+(
+    ItemSALKey            nvarchar(255) not null
+        constraint ItemSAL_pk
+            primary key,
+    MasterItemHubKey      nvarchar(255) not null,
+    OperationalItemHubKey nvarchar(255) not null,
+    RecordSourceCode      nvarchar(50)  not null,
+    LoadDT                datetime2     not null
+)
+go
+
